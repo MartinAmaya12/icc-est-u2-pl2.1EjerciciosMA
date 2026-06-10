bbec82a7fbd3628ed41d1049d5c2314f667762f1
Gabriel Cuenca <gcuenca119@gmail.com>
2026-06-10 18:05:42 -0500

    ADD : Ejercicio 3 Queue Palindrome en Java

--- src/App.java
+++ src/App.java
@@ -1,3 +1,7 @@
+import utils.SignValidator;
+import utils.StackSorter;
+import utils.QueuePalindrome;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("\nEstudiante: Martin Amaya\n");
--- src/utils/QueuePalindrome.java
+++ src/utils/QueuePalindrome.java
@@ -1,5 +1,32 @@
 package utils;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class QueuePalindrome {
+    public boolean esPalindromo(String texto) {
+        Queue<Character> cola = new LinkedList<>();
+        Stack<Character> pila = new Stack<>();
+
+        for (char letra : texto.toCharArray()) {
+            cola.add(letra);  
+            pila.push(letra); 
+        }
+
+  
+        while (!cola.isEmpty()) {
+   
+            char deLaCola = cola.remove(); 
+            char deLaPila = pila.pop();    
+
     
+            if (deLaCola != deLaPila) {
+                return false; 
+            }
+        }
+
+
+        return true;
+    }
 }
